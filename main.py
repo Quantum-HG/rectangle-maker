66131340c2eba833ad7587fbb9378a5c1e365886
Quantum-HG <119344859+Quantum-HG@users.noreply.github.com>
2024-05-21 20:37:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,20 +22,16 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 '''
 
-
-
-
-
-# b-> width of the rectangle
-# a -> height of the rectangle
+# a-> width of the rectangle
+# b -> height of the rectangle
 def make_rectangle(a, b):
     """
     Makes a rectangle of dimensions axb
-    a: height of rectangle
-    b: width of rectangle
+    a: width of rectangle
+    b: height of rectangle
     
     for e.g.
-        make_rectangle(5, 15)
+        make_rectangle(15, 5)
         
         >>> 
             ooooooooooooooo
@@ -45,12 +41,12 @@ def make_rectangle(a, b):
             ooooooooooooooo
     
     """
-    for i in range(a):
-        for j in range(b):
-            if i == 0 or i == (a - 1):
+    for i in range(b):
+        for j in range(a):
+            if i == 0 or i == (b - 1):
                 print("o", end="")
             else:
-                if j == 0 or j == (b - 1):
+                if j == 0 or j == (a - 1):
                     print("o", end="")
                 else:
                     print(" ", end="")
@@ -63,4 +59,4 @@ def make_rectangle(a, b):
 
 
 if __name__ == '__main__':
-    make_rectangle(5, 15) # 5x15 
+    make_rectangle(15, 5) # 15x5
